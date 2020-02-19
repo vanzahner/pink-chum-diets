9b85aacc165cec27e3b67e5e2cc4d910bd21eac4
vanflad <vanessa40_40@hotmail.com>
2020-02-18 17:33:17 -0800

    zoop data file updated

--- scripts/envr_zoop_data.R
+++ scripts/envr_zoop_data.R
@@ -18,12 +18,11 @@ ysi_filtered <- ysi_data %>%
                         "J02", "J06", "J08") & 
            survey_date>"2015-05-20"&
            survey_date<"2016-07-06")
-#need to filter out to only what's relevant to samples (then secchi and zoops next)
+#need to filter out to only what's relevant to samples (then secchi next)
 
-zoop_data_taxa <- read_csv("data/zoop_data_taxa.csv")
+zoop_data <- read_csv("data/zoop_comp_data_combined.csv")
+#need to resolve issues about missing data (JSPK 1154, which is for July 5, 2016 J07)
 
-zoop_data_ww <- read_csv("data/zoop_data_ww.csv")
+#zoop_data_ww <- read_csv("data/zoop_data_ww.csv")
+#J02 (JSPK1118) has taxa data but no wet weight. ignore all ww since J02 most important
 
-#need to resolve issues about missing data/duplicates. JSPK1093 and see notes for others.
-#and hippotylidae zoea blank issue too. AND size class saving as date... ERGH!
-#and add location (DI or JS); biomass; volume corrected; etc. to my version of zoop data
